ddba7961da491679d6c9d4fd057ffc06c85de230
Atanas Pashkov <dgaduin@gmail.com>
2020-06-09 14:50:14 +0100

    Add test for summing items

--- CourierKataTest/OrderTests.cs
+++ CourierKataTest/OrderTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using CourierKata.Domain;
 using FluentAssertions;
 using FluentAssertions.Execution;
@@ -6,5 +7,20 @@ using Xunit;
 
 namespace CourierKata.Test
 {
-    public class OrderTests { }
+    public class OrderTests
+    {
+        [Fact]
+        public void Order_should_sum_the_parcels_in_it()
+        {
+            IList<IOrderItem> items = new List<IOrderItem> {
+                new Parcel(10, 150, 2), //XL
+                new Parcel(10, 74, 2), //L
+                new Parcel(12, 4, 2), //M
+                new Parcel(4, 4, 2) //S
+            };
+            var order = new Order(items);
+            order.SumOrderItems();
+            order.TotalCost.Should().Be(51, "25+15+8+3=51");
+        }
+    }
 }
