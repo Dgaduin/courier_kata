1f96ab361955b82ed89f78ff2bfae165baf99332
Atanas Pashkov <dgaduin@gmail.com>
2020-06-09 13:51:51 +0100

    Implement logic for XL Parcel sizes

--- CourierKataDomain/Parcel.cs
+++ CourierKataDomain/Parcel.cs
@@ -10,13 +10,31 @@ namespace CourierKata.Domain
 
         public Parcel(int height, int width, int length)
         {
-            if (height <= 0) throw new ArgumentOutOfRangeException(nameof(height),"Value needs to be positive");
-            if (width <= 0) throw new ArgumentOutOfRangeException(nameof(width),"Value needs to be positive");
-            if (length <= 0) throw new ArgumentOutOfRangeException(nameof(length),"Value needs to be positive");
+            if (height <= 0) throw new ArgumentOutOfRangeException(nameof(height), "Value needs to be positive");
+            if (width <= 0) throw new ArgumentOutOfRangeException(nameof(width), "Value needs to be positive");
+            if (length <= 0) throw new ArgumentOutOfRangeException(nameof(length), "Value needs to be positive");
 
-            Height=height;
-            Width=width;
-            Length=length;
+            Height = height;
+            Width = width;
+            Length = length;
         }
+
+        public ParcelSize Size
+        {
+            get
+            {
+                if (Height >= 100 || Width >= 100 || Length >= 100)
+                    return ParcelSize.Xl;
+                return ParcelSize.S;
+            }
+        }
+    }
+
+    public enum ParcelSize
+    {
+        S = 2,
+        M = 3,
+        L = 4,
+        Xl = 5
     }
 }
