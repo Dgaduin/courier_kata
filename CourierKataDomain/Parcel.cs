2660c989e288fdf081146cecd0d93a73ded17fdc
Atanas Pashkov <dgaduin@gmail.com>
2020-06-09 13:40:39 +0100

    Implement validation for width and length

--- CourierKataDomain/Parcel.cs
+++ CourierKataDomain/Parcel.cs
@@ -11,6 +11,12 @@ namespace CourierKata.Domain
         public Parcel(int height, int width, int length)
         {
             if (height <= 0) throw new ArgumentOutOfRangeException(nameof(height),"Value needs to be positive");
+            if (width <= 0) throw new ArgumentOutOfRangeException(nameof(width),"Value needs to be positive");
+            if (length <= 0) throw new ArgumentOutOfRangeException(nameof(length),"Value needs to be positive");
+
+            Height=height;
+            Width=width;
+            Length=length;
         }
     }
 }
