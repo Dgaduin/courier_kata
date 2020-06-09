c0e263136267b65d09983b590459195bcfebc799
Atanas Pashkov <dgaduin@gmail.com>
2020-06-09 13:33:35 +0100

    Add validation for height<0

--- CourierKataTest/ParcelTests.cs
+++ CourierKataTest/ParcelTests.cs
@@ -1,10 +1,20 @@
 using System;
+using CourierKata.Domain;
+using FluentAssertions;
 using Xunit;
 
 namespace CourierKata.Test
 {
     public class ParcelTests
     {
-       
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-27)]
+        [InlineData(-524)]
+        public void Parcel_with_negative_height_should_throw_Argument_out_of_range(int height)
+        {
+            Action a = () => new Parcel(height, 1, 1);
+            a.Should().ThrowExactly<ArgumentOutOfRangeException>().WithMessage("Value needs to be positive (Parameter 'height')");
+        }        
     }
 }
